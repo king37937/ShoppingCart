af44778149bb8c2510f42d907d6906694bbf8997
king.ho99 <king.ho@titansoft.com.sg>
2017-06-19 22:12:36 +0800

    init project

---
+++ ShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void CalculatePriceTest()
+        {
+        }
+    }
+}
